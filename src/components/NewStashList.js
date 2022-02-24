51da92e8a9d4666d9e792cae8c3a2d79b77cf346
Margaret Farron <mpnauss@gmail.com>
2022-02-24 12:11:54 -0800

    Fix all the things

--- src/components/NewStashList.js
+++ src/components/NewStashList.js
@@ -4,16 +4,48 @@ import YarnShow from "./YarnShow"
 
 const NewStashList = (props) => {
 
-    const [stashList, setStashList] = useState([1, 2, 3, 4, 5])
+    const [stashList, setStashList] = useState([])
     const [fullStash, setFullStash] = useState([])
     const [weightYards, setWeightYards] = useState({})
 
+    const url = window.location.hash
+    const access_token = url.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
+
+    const getStashList = async () => {
+        try {
+            const stashListResponse = await fetch('https://api.ravelry.com/stash/search.json?query=fringerella&stash-status=stash',
+                {
+                    method: 'GET',
+                    headers: {
+                        Authorization: "Bearer " + access_token
+                    },
+                    mode: 'cors'
+                })
+            if (!stashListResponse.ok) {
+                const errorMessage = `${stashListResponse.status} (${stashListResponse.statusText})`
+                const error = new Error(errorMessage)
+                throw (error)
+            }
+            const stashData = await stashListResponse.json()
+            setStashList(stashData.stashes)
+        } catch (err) {
+            console.error(`Error in fetch: ${err.message}`)
+        }
+    }
+
+    useEffect(() => {
+        getStashList()
+    }, [])
 
     const getFullStashData = async (data) => {
         try {
             const reqs = data.map(stashItem => {
-                const stashResponse = fetch(`http://localhost:8000/stash/${stashItem}`, {
-                    method: 'GET'
+                const stashResponse = fetch(`https://api.ravelry.com/people/fringerella/stash/${stashItem.id}.json`, {
+                    method: 'GET',
+                    headers: {
+                        Authorization: "Bearer " + access_token
+                    },
+                    mode: 'cors'
                 })
                 return stashResponse
             })
@@ -22,9 +54,11 @@ const NewStashList = (props) => {
             
             const tempWeightObj = {}
             fullStashArray.forEach(item => {
-                let stashedYards = item.amount_yards
-                Object.defineProperty(tempWeightObj, item.weight_name, {value: stashedYards, writable: true })
-
+                let stashedYards = item.stash.packs[0].total_yards
+                let weightName = item.stash.yarn_weight_name
+                Object.defineProperty(tempWeightObj, item.stash.yarn_weight_name, {value: stashedYards, writable: true })
+                // setWeightYards({weightName: stashedYards, ...weightYards})
+                // console.log(weightYards)
             })
             console.log(tempWeightObj)
             setWeightYards(tempWeightObj)
